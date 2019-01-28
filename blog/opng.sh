6d0a056e42088be53006abafe9e1a23c5a443823
Bernie Lim <limbernie@pm.me>
2019-01-28 01:32:17 +0000

    add watermark

--- blog/opng.sh
+++ blog/opng.sh
@@ -4,14 +4,14 @@ TEMP=$(mktemp -u)
 
 for png in *.png; do
 	echo $png
-#	height=$(identify $png | cut -d' ' -f3 | cut -d'x' -f2)
-#	if [ $height -lt 100 ]; then
-#		convert -resize $height ../stamp.png - | composite -gravity West - $png $png
-#	elif [ $height -lt 320 ]; then
-#		convert -resize $height ../stamp.png - | composite -gravity Center - $png $png
-#	else
-#		composite -gravity Center ../stamp.png $png $png
-#	fi
+	height=$(identify $png | cut -d' ' -f3 | cut -d'x' -f2)
+	if [ $height -lt 100 ]; then
+		convert -resize $height ../stamp.png - | composite -gravity West - $png $png
+	elif [ $height -lt 320 ]; then
+		convert -resize $height ../stamp.png - | composite -gravity Center - $png $png
+	else
+		composite -gravity Center ../stamp.png $png $png
+	fi
 	convert -strip -alpha Remove $png $png
 	pngquant $png
 done > $TEMP
