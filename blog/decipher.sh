c372cf8797d2afe76853c349c8a815b49c8c8bb1
Bernie Lim <limbernie@pm.me>
2019-05-06 06:36:39 +0000

    change encipher to decipher

--- blog/decipher.sh
+++ blog/decipher.sh
@@ -5,7 +5,7 @@ read -p "Password: " PASS
 TEMP=$(mktemp -u)
 echo -n $PASS | sha512sum | cut -d' ' -f1 > $TEMP
 
-# encipher images
+# decipher images
 for png in *.png; do
 	convert -decipher $TEMP $png $png
 done
