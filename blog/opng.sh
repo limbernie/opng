4ee9cf6ce28fdac809f6e17e449f3947140155ca
Bernie Lim <limbernie@pm.me>
2019-05-06 01:16:29 +0000

    no more copyright notices. yeah!

--- blog/opng.sh
+++ blog/opng.sh
@@ -1,26 +1,9 @@
 #!/bin/bash
 
-# Front matter
-OPNG=$(readlink -f `dirname $0`)
 TEMP=$(mktemp -u)
-WATERMARK=watermark.sh
 
-# Generate the stamp
-cd ..
-./$WATERMARK
-
-# Stamp it
-cd $OPNG
 for png in *.png; do
 	echo $png
-	height=$(identify $png | cut -d' ' -f3 | cut -d'x' -f2)
-	if [ $height -lt 100 ]; then
-		convert -resize $height ../stamp.png - | composite -gravity West - $png $png
-	elif [ $height -lt 320 ]; then
-		convert -resize $height ../stamp.png - | composite -gravity Center - $png $png
-	else
-		composite -gravity Center ../stamp.png $png $png
-	fi
 	convert -strip -alpha Remove $png $png
 	pngquant $png
 done > $TEMP
---
+++ blog/stamp.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Front matter
+OPNG=$(readlink -f `dirname $0`)
+TEMP=$(mktemp -u)
+WATERMARK=watermark.sh
+
+# Generate the stamp
+cd ..
+./$WATERMARK
+
+# Stamp it
+cd $OPNG
+for png in *.png; do
+	echo $png
+	height=$(identify $png | cut -d' ' -f3 | cut -d'x' -f2)
+	if [ $height -lt 100 ]; then
+		convert -resize $height ../stamp.png - | composite -gravity West - $png $png
+	elif [ $height -lt 320 ]; then
+		convert -resize $height ../stamp.png - | composite -gravity Center - $png $png
+	else
+		composite -gravity Center ../stamp.png $png $png
+	fi
+	convert -strip -alpha Remove $png $png
+	pngquant $png
+done > $TEMP
+
+for png in $(cat $TEMP); do
+	rm $png
+	mv "${png%*.png}-fs8.png" $png
+done
+
+rm -f $TEMP
