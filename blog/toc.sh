a0b279e93c032ccc7df359445d54a51f05458417
Bernie Lim <limbernie@pm.me>
2019-10-26 13:24:00 +0000

    new workflow: add toc to post

---
+++ blog/toc.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+FILES=$@
+
+for md in $(readlink -f $FILES); do
+
+if grep '\{:toc\}' $md &>/dev/null; then
+  exit
+fi
+
+sed -ir '/<!--more-->/a \
+\
+## On this post \
+{:.no_toc} \
+\
+* TOC \
+{:toc}' $mr
+
+done
