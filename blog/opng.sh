5d8c4fb57ca205425fb5f814f36305e459d9d763
Bernie Lim <limbernie@pm.me>
2020-04-07 15:38:05 +0000

    responsive images

--- blog/opng.sh
+++ blog/opng.sh
@@ -4,10 +4,13 @@ TEMP=$(mktemp -u)
 
 for png in *.png; do
 	echo "${png}"
+    echo "${png%*.png}-medium.png"
     echo "${png%*.png}-small.png"
 	convert -strip -alpha Remove "${png}" "${png}"
+    convert -resize 66% "${png}" "${png%*.png}-medium.png"
     convert -resize 33% "${png}" "${png%*.png}-small.png"
 	pngquant "${png}"
+    pngquant "${png%*.png}-medium.png"
     pngquant "${png%*.png}-small.png"
 done > $TEMP
 
