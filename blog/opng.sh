4e289c7cbdc5d8e55f9870a99cc9de576895836f
Bernie Lim <limbernie@pm.me>
2020-04-07 15:11:54 +0000

    responsive images

--- blog/opng.sh
+++ blog/opng.sh
@@ -3,9 +3,12 @@
 TEMP=$(mktemp -u)
 
 for png in *.png; do
-	echo $png
-	convert -strip -alpha Remove $png $png
-	pngquant $png
+	echo "${png}"
+    echo "${png%*.png}-small.png"
+	convert -strip -alpha Remove "${png}" "${png}"
+    convert -resize 33% "${png} "${png%*.png}-small.png"
+	pngquant "${png}"
+    pngquant "${png%*.png}-small.png"
 done > $TEMP
 
 for png in $(cat $TEMP); do
--- blog/optimize-jpg.sh
+++ blog/optimize-jpg.sh
@@ -3,11 +3,20 @@
 FILES=$@
 
 for img in $(readlink -f $FILES); do
-  convert -strip \
-          -sampling-factor 4:2:0 \
-          -quality 85 \
-          -interlace JPEG \
-          -colorspace sRGB \
-          -resize 800 \
-          $img $img
+    if [ "${img#*.}" == "jpg" ]; then
+        convert -strip \
+                -sampling-factor 4:2:0 \
+                -quality 85 \
+                -interlace JPEG \
+                -colorspace sRGB \
+                -resize 800 \
+                "${img}" "${img}" &&
+        convert -resize 33% "${img}" "${img%*.jpg}-small.jpg" &&
+    else
+        convert -resize 800 \
+                -strip \
+                -alpha Remove \
+                "${img}" "${img}" &&
+        convert -resize 33% "${img}" "${img%*.png}-small.png"
+    fi
 done
