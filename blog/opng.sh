e52925f6536d0b9142615f3cc73c7f783d42c22a
Bernie Lim <limbernie@pm.me>
2020-04-07 15:16:57 +0000

    responsive images

--- blog/opng.sh
+++ blog/opng.sh
@@ -6,14 +6,14 @@ for png in *.png; do
 	echo "${png}"
     echo "${png%*.png}-small.png"
 	convert -strip -alpha Remove "${png}" "${png}"
-    convert -resize 33% "${png} "${png%*.png}-small.png"
+    convert -resize 33% "${png}" "${png%*.png}-small.png"
 	pngquant "${png}"
     pngquant "${png%*.png}-small.png"
 done > $TEMP
 
 for png in $(cat $TEMP); do
-	rm $png
-	mv "${png%*.png}-fs8.png" $png
+	rm "${png}"
+	mv "${png%*.png}-fs8.png" "${png}"
 done
 
 rm -f $TEMP
