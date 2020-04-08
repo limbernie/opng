c7a450b7ea7d268c065061552f1d45f65132b3f1
Bernie Lim <limbernie@pm.me>
2020-04-08 07:46:16 +0000

    responsive image

--- blog/opng.sh
+++ blog/opng.sh
@@ -8,15 +8,13 @@ for png in *.png; do
     pngquant "${png}"
 
     if [ "$(identify -format %w "${png}")" -ge 800 ]; then
-        echo "${png%*.png}-320w.png"
-        echo "${png%*.png}-480w.png"
-        echo "${png%*.png}-600w.png"
-        convert -resize 320 "${png}" "${png%*.png}-320w.png"
-        convert -resize 480 "${png}" "${png%*.png}-480w.png"
-        convert -resize 600 "${png}" "${png%*.png}-600w.png"
-        pngquant "${png%*.png}-320w.png"
-        pngquant "${png%*.png}-480w.png"
-        pngquant "${png%*.png}-600w.png"
+        echo "${png%*.png}-s.png"
+        convert -resize 480 "${png}" "${png%*.png}-s.png"
+        pngquant "${png%*.png}-s.png"
+    else
+        echo "${png%*.png}-s.png"
+        cp "${png}" "${png%*.png}-s.png"
+        pngquant "${png%*.png}-s.png"
     fi
 done > $TEMP
 
