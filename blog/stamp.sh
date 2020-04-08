0e1e8909c4e71368e427061964d857c60fa11b42
Bernie Lim <limbernie@pm.me>
2020-04-08 11:17:42 +0000

    fine-tune blog workflow

--- blog/stamp.sh
+++ blog/stamp.sh
@@ -6,7 +6,6 @@ TEMP=$(mktemp -u)
 WATERMARK=watermark.sh
 
 # Generate the stamp
-cd ..
 ./$WATERMARK
 
 # Stamp it
@@ -15,11 +14,11 @@ for png in *.png; do
 	echo $png
 	height=$(identify $png | cut -d' ' -f3 | cut -d'x' -f2)
 	if [ $height -lt 100 ]; then
-		convert -resize $height ../stamp.png - | composite -gravity West - $png $png
+		convert -resize $height stamp.png - | composite -gravity West - $png $png
 	elif [ $height -lt 320 ]; then
-		convert -resize $height ../stamp.png - | composite -gravity Center - $png $png
+		convert -resize $height stamp.png - | composite -gravity Center - $png $png
 	else
-		composite -gravity Center ../stamp.png $png $png
+		composite -gravity Center stamp.png $png $png
 	fi
 	convert -strip -alpha Remove $png $png
 	pngquant $png
