1f3d240c791ce36b349c7d9f0024249628064a03
Bernie Lim <limbernie@pm.me>
2020-04-08 03:06:18 +0000

    responsive image

--- blog/opng.sh
+++ blog/opng.sh
@@ -12,7 +12,7 @@ for png in *.png; do
         echo "${png%*.png}-480w.png"
         echo "${png%*.png}-600w.png"
         convert -resize 320 "${png}" "${png%*.png}-320w.png"
-        convert -resize 480 "${png}" "${png%*.png}-480vw.png"
+        convert -resize 480 "${png}" "${png%*.png}-480w.png"
         convert -resize 600 "${png}" "${png%*.png}-600w.png"
         pngquant "${png%*.png}-320w.png"
         pngquant "${png%*.png}-480w.png"
