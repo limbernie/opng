a3220acb30816b4114819a6d6c2b4d04ad213f51
Bernie Lim <limbernie@pm.me>
2020-04-09 06:28:27 +0000

    add webp support

---
+++ blog/optimize-img.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+FILES=$@
+
+for img in $(readlink -f $FILES); do
+    if [ "${img#*.}" == "jpg" ]; then
+        convert -strip \
+                -sampling-factor 4:2:0 \
+                -quality 85 \
+                -interlace JPEG \
+                -colorspace sRGB \
+                -resize 800 \
+                "${img}" "${img}" &&
+        cwebp "${img}" -o "${img%*.jpg}".webp
+    else
+        convert -resize 800 \
+                -strip \
+                -alpha Remove \
+                "${img}" "${img}" &&
+        cwebp "${img}" -o "${img%*.png}".webp
+    fi
+done
